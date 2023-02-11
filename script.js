b273f92b13306e33f77f87032866de2e6efe072b
Marco <patrickstar1281@yahoo.com>
2023-02-10 19:46:19 -0600

    enemy1 movement done

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = 'enemy1.png';
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
         // we want the speed to be between -2 and 4. The (Math.random() * 4) part generates a random number between 0 - 4 
         // while the ( - 2) part at the end tells the program to start this list from -2.
         // So now we have a random number between -2 and 4.
-        this.speed = Math.random() * 4 - 2;
+        //this.speed = Math.random() * 4 - 2;
         // width & height of the area that is cut out of sprite image for animations
         this.spriteWidth = 293
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
 
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         //animate sprites
         if (gameFrame % this.flapSpeed === 0) {
             //if this.frame is greater than 4 then reset back to zero otherwise increase this.frame
